ac249cd77e2a94ae653805102cb8c710f9a391fb
Jennifer <jennifer.elaine.mairead@gmail.com>
2017-12-12 15:46:29 +0000

    Adapted to print index and print students who start with a particular letter

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 #let's put all the students into an array
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+#students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name: "Darth Vader", cohort: :november},
+#  {name: "Nurse Ratched", cohort: :november},
+#  {name: "Michael Corleone", cohort: :november},
+#  {name: "Alex DeLarge", cohort: :november},
+#  {name: "The Wicked Witch of the West", cohort: :november},
+#  {name: "Terminator", cohort: :november},
+#  {name: "Freddy Kreuger", cohort: :november},
+#  {name: "The Joker", cohort: :november},
+#  {name: "Joffrey Baratheon", cohort: :november},
+#  {name: "Norman Bates", cohort: :november}
+#]
 
 #Now let's print them
 def print_header
@@ -20,9 +20,15 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print(students, letter)
+  select_students = []
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].start_with? letter
+      select_students << student
+    end
+  end
+  select_students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -30,6 +36,22 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+  #create an empty array
+  students = []
+  #request first name
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts  "Now we have #{students.length} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header
-print(students)
+print(students, "c")
 print_footer(students)
