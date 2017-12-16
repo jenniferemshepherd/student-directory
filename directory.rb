9aa3e0b6de6ec8dd02793f3e6db1afa68411f6e4
Jennifer <jennifer.elaine.mairead@gmail.com>
2017-12-16 17:37:50 +0100

    Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -14,80 +14,87 @@
 #  {name: "Norman Bates", cohort: :november}
 #]
 
-#Now let's print them
+@students = []
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
+def print_students
   n = 1
-  while n <= students.length
-    puts "#{n}. " + students[n-1][:name].to_s + " (" + students[n-1][:cohort].to_s + " cohort)"
+  while n <= @students.length
+    puts "#{n}. " + @students[n-1][:name].to_s + " (" + @students[n-1][:cohort].to_s + " cohort)"
     n +=1
   end
 end
 
-def print_footer(students)
-  if students.count == 1
+def print_footer
+  if @students.count == 1
     puts "Overall, we have 1 great student"
   else
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
-  #create an empty array
-  students = []
   #request first name
   puts "Type name:"
-  name = gets.sub("\n","")
+  name = gets.chomp
   while !name.empty? do
     puts "Type cohort:"
-    cohort = gets.delete("\n")
+    cohort = gets.chomp
     cohort = "november" if cohort == ""
-    students << {name: name, cohort: cohort, hobbies: :cycling, country_of_birth: :UK}
-    if students.length == 1
-      puts "Now we have 1 student"
+    @students << {name: name, cohort: cohort, hobbies: :cycling, country_of_birth: :UK}
+    if @students.length == 1
+      puts "Now we have 1 student."
     else
-      puts  "Now we have #{students.length} students"
+      puts  "Now we have #{@students.length} students."
     end
     puts "Type name:"
-    name = gets.sub("\n","")
+    name = gets.chomp
   end
-  students
+  @students
 end
 
 #choose a letter for sorting students by
 letter = "C"
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    puts "What would you like to do?"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      if students.count == 0
-        puts "There are no students enrolled."
-      else
-        print_header
-        print(students)
-        print_footer(students)
-      end
-    when "9"
-      break
-    else
-      puts "I don't know what you meant, please try again."
-    end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  puts "What would you like to do?"
+end
+
+def show_students
+  if @students.count == 0
+    puts "There are no students enrolled."
+  else
+    print_header
+    print_students
+    print_footer
   end
 end
 
-interactive_menu
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    break
+  else
+    puts "I don't know what you meant, please try again."
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
